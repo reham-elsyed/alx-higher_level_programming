ba5358f1600febb65073c7e57ffdb5d83751a526
reham-elsyed <rehamshepl666@gmail.com>
2024-01-03 10:06:18 -0800

    python print

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(ord('a'), ord('z') + 1):
-    if a(letter) != 'e' and a(letter) != 'q':
+    if chr(letter) != 'e' and chr(letter) != 'q':
         print('{:c}'.format(letter), end='')
