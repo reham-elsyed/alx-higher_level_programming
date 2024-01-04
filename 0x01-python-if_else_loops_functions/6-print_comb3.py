3ff9eb4e05e765bd80e151fb89aebd94c52a498a
reham-elsyed <rehamshepl666@gmail.com>
2024-01-04 08:42:59 -0800

    print digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for num in range(0, 10):
+    for num in range(x + 1, 10):
+        if x == 8 and y == 9:
+            print('98')
+        else:
+            print('{}{}'.format(x, y), end='')
