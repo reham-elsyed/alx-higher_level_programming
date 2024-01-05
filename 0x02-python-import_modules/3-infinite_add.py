077831969f0554ab5b822d2c98f4ebee3c55d707
reham-elsyed <rehamshepl666@gmail.com>
2024-01-05 09:05:36 -0800

    argc

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     total = 0
-    if  len(sys.argv) - 1 == 0:
+    if (len(sys.argv) - 1) == 0:
         print("0")
     else:
         for i in range(len(sys.argv) - 1):
