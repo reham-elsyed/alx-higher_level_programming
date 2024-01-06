f7b12c5fe732fd017c3f26f567b6ee9447e1326b
reham-elsyed <rehamshepl666@gmail.com>
2024-01-06 07:15:46 -0800

    ff

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    if idx >= len(my_list):
+    elif idx >= len(my_list):
         return None
-    
-    return my_list[index]
+ 
+    return my_list[idx]
