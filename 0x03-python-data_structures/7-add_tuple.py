9905db9e93aadb35b5c6c97ba29785ee488446e3
reham-elsyed <rehamshepl666@gmail.com>
2024-01-07 11:19:18 -0800

    tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_c = (0, 0)
+    if len(tuple_a) > 0 and len(typle_b) > 0
+    tuple_c[0] = tuple_a[0] + tuple_b[0]
+    tuple_c[1] = tuple_a[1] + tuple_b[1]
+    return tuple_c
