212d359e42676a27b36a4cb88fa156c7ae47ee44
reham-elsyed <rehamshepl666@gmail.com>
2024-01-07 05:40:27 -0800

    k

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    print("matrix = [", end="")
     for i in matrix:
+        print("\n [", end="")
         for j in i:
-            print("{:d}".format(j), end=" ")
-        print("")
+            print("{:d}, ".format(j), end="")
+        print("],")
+    print("")
