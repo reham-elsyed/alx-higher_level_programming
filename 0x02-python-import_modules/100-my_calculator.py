c12e54d938e11e8c971a3d87ba74969c8074885b
reham-elsyed <rehamshepl666@gmail.com>
2024-01-09 08:49:47 -0800

    cal

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 if __name__ = "__main__":
+    import sys
     from calculator_1 import add, sub, mul, div
     a_cast = int(sys.argv[1])
-    b_cast =int(sys.argv[2])
-    operator =['+', '_', '*', '/']
+    b_cast =int(sys.argv[3])
+    s = sys.argv[2]
+
     if len(sys.argv) > 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit 1
     else:
-        if s not in operator:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
         if s == '+':
             print("{:d} {} {:d} = {:d}.format(a_cast, s.operator, b_cast, add(a_cast, b_cast))")
         elif s == '_':
@@ -19,3 +18,6 @@ if __name__ = "__main__":
             print("{:d} {} {:d} = {:d}.format(a_cast, s.operator, b_cast, mul(a_cast, b_cast))")
         elif s == '/':
             print("{:d} {} {:d} = {:d}.format(a_cast, s.operator, b_cast, div(a_cast, b_cast))")
+        else:
+             print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
