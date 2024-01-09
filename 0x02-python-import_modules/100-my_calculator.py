04524c5135f8542ed4ebc47dff577dc916423812
reham-elsyed <rehamshepl666@gmail.com>
2024-01-09 09:15:44 -0800

    cal

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,22 +2,16 @@
 if __name__ = "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    a_cast = int(sys.argv[1])
-    b_cast =int(sys.argv[3])
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     s = sys.argv[2]
 
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit 1
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if s not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
     else:
-        if s == '+':
-            print("{:d} '+' {:d} = {:d}.format(a_cast, b_cast, add(a_cast, b_cast))")
-        elif s == '_':
-            print("{:d} '_' {:d} = {:d}.format(a_cast, , b_cast, sub(a_cast, b_cast))")
-        elif s == '*':
-            print("{:d} '*' {:d} = {:d}.format(a_cast, b_cast, mul(a_cast, b_cast))")
-        elif s == '/':
-            print("{:d} '/' {:d} = {:d}.format(a_cast, b_cast, div(a_cast, b_cast))")
-        else:
-             print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit 1
+        print("{} {} {} = {}".format(a, s, b, ops[sys.argv[2]](a, b)))
