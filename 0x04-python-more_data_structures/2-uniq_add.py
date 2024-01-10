beabb319925c52330545624fb8072a144d41a249
reham-elsyed <rehamshepl666@gmail.com>
2024-01-10 06:51:59 -0800

    replace

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_list = my_list.copy()
-    new_list = list(set(new_list))
-    new_list.sort()
-    sum = 0
-    for i in range(len(new_list) + 1):
-        sum += i
-    return sum
+    new_list = sorted(set(my_list))
+    return sum(new_list)
