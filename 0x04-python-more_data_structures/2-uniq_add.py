923c1e4f73503393c681e026c032db5ffb999ed7
reham-elsyed <rehamshepl666@gmail.com>
2024-01-10 06:32:28 -0800

    replace

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = my_list.copy()
+    new_list = list(set(new_list))
+    new_list.sort()
+    sum = 0
+    for i in range(len(new_list) + 1):
+        sum += 1
+    return sum
