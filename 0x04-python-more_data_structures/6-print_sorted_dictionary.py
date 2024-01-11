0f34a619d6916e80546e0dd38fcab0417b532781
reham-elsyed <rehamshepl666@gmail.com>
2024-01-11 00:05:50 -0800

    odd

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted_keys = sorted(a_dictionary.keys())
+    sorted_keys = sorted(a_dictionary.keys(), key=str.lower)
     sorted_dict = {k: a_dictionary[k] for k in sorted_keys}
     print(sorted_dict)
