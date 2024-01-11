bf70b6baacdcec79f82f286234fb8c65ff68dcc2
reham-elsyed <rehamshepl666@gmail.com>
2024-01-10 23:48:07 -0800

    odd

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/pythos3
+def print_sorted_dictionary(a_dictionary):
+    sorted_keys = sorted(a_dictionary.keys())
+    sorted_dict = {k: a_dictionary[k] for k in sorted_keys}
+    print(sorted_dict)
