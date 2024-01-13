ba5f6cf3051f2a62945afe5af9b9693e2543ecdd
reham-elsyed <rehamshepl666@gmail.com>
2024-01-13 10:24:00 -0800

    l

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if value  not in a_dictionary.values():
+    if value not in a_dictionary.values():
         return a_dictionary
     else:
         key_del = [key for key, val in a_dictionary.items() if val == value]
