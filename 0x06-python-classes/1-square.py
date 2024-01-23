cb6c0930370c6c5ced701cb602d293d84936cb58
reham-elsyed <rehamshepl666@gmail.com>
2024-01-23 08:49:08 -0800

    m

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-""" Sqaure class"""
+""" Sqaure class."""
+
 class Square:
+    """Define a square"""
+
     def _init__(self, size):
+        """Constructor.
+        Args:
+        size: length.
+        """
         self.__size = size
