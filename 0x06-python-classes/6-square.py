5dfa50ef52b561fdfafd0584a2b9fa223caccc72
reham-elsyed <rehamshepl666@gmail.com>
2024-01-24 06:50:38 -0800

    p

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -30,7 +30,7 @@ class Square:
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
-            raise ValueError("size must br >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     @property
