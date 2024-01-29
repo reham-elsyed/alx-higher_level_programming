b5cf8fd89a96e93c643558a92c5558ce4b03917e
reham-elsyed <rehamshepl666@gmail.com>
2024-01-29 13:28:57 -0800

    l

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
+"""Module for say_my_name method"""
+
 
 def say_my_name(first_name, last_name=""):
-    if not isinstance(first_name, str) or not first_name:
+    """Method for printing first and last name.
+
+    Args:
+        first_name: first name input.
+        last_name: last name input.
+
+    Raise:
+        TypeError: if first and last name not string.
+    """
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
