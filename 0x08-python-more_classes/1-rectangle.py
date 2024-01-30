b350716ea656f2d13a5974ddde764b91c80270f1
reham-elsyed <rehamshepl666@gmail.com>
2024-01-30 11:10:41 -0800

    rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,8 +13,8 @@ class Rectangle:
         width: width
         height: height
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
