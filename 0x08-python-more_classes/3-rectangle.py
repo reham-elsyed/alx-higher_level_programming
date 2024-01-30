0dde7b30d134c875b8bef1e3dbe567b34d764e66
reham-elsyed <rehamshepl666@gmail.com>
2024-01-30 14:57:38 -0800

    square area

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""" Define Class"""
+
+
+class Rectangle:
+    """ Class rectangle"""
+    def __init__(self, width=0, height=0):
+        """constructor
+
+        Args:
+            width:width input
+            height: height input
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__height + self.__width) * 2
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        result = ""
+        for i in range (self.__height):
+            for j in range (self.__width):
+                result += "#"
+            result += '\n'
+        return result
