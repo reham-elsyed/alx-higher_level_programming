9509cd31e8a782263a93acbf166b0f9f050b86a3
reham-elsyed <rehamshepl666@gmail.com>
2024-02-04 09:41:40 -0800

    print int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,5 +4,5 @@ def safe_print_integer(value):
         if isinstance(value, int):
             print("{:d}".format(value))
             return True
-    except ValueError:
+    except (ValueError, TypeError):
         return False
