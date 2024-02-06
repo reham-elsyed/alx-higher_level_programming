931c052ee5989d25e6836d93edbeee981fb8f9d0
reham-elsyed <rehamshepl666@gmail.com>
2024-02-06 10:59:31 -0800

    hm

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Define text file reading function"""
+"""Defines a text file-reading function."""
 
 
 def read_file(filename=""):
-    """Method to read file"""
-    with open(filename, encoding="UTF-8") as f:
-        print(f.read(), end='')
+        """Print the contents of a UTF8 text file to stdout."""
+        with open(filename, encoding="utf-8") as f:
+            print(f.read(), end="")
