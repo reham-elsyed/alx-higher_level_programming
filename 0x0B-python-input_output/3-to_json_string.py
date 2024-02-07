0ae634c83fe06f5a0886314f611ee8430afe8eba
reham-elsyed <rehamshepl666@gmail.com>
2024-02-07 01:50:33 -0800

    json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define method to stringfy json"""
+
+
+def to_json_string(my_obj):
+    """Function to string json
+
+    Args:
+        my_obj: object
+
+    Return:
+        json rep of object
+    """
+    return json.dumps(my_obj)
