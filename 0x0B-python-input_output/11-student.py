2ccade984f5b7a06ed58eeaa0c573da41fa7973d
reham-elsyed <rehamshepl666@gmail.com>
2024-02-07 15:52:08 -0800

    k

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -10,7 +10,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attr=None):
+    def to_json(self, attrs=None):
         if attrs is None:
 
             attrs = vars(self).keys()
