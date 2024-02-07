37b27d67b769aa2bb99427fa2f280fa061c92f4c
reham-elsyed <rehamshepl666@gmail.com>
2024-02-07 13:08:13 -0800

    p

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -12,6 +12,6 @@ def class_to_json(obj):
     new_dict = {}
     obj_att = vars(obj)
     for key, value in obj_att.items():
-        if isinstance(value, (lisr, int, str, bool, dict)):
+        if isinstance(value, (list, int, str, bool, dict)):
             new_dict[key] = value
     return new_dict
