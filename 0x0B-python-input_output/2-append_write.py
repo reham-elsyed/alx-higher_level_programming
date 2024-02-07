a5a11b8a7cffb826e9f3bd06a3e73bd3ff030abb
reham-elsyed <rehamshepl666@gmail.com>
2024-02-07 01:42:34 -0800

    append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""Method to write to file"""
+
+
 def append_write(filename="", text=""):
+    """Function to append text to file.
+
+    Args:
+        filename: file
+        text: text to be appended.
+    Return:
+        length of text
+    """
     with open(filename, 'a', encoding="UTF-8") as f:
         return f.write(text)
