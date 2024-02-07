ea415e7677cd7197627ca07081191e63243be9c3
reham-elsyed <rehamshepl666@gmail.com>
2024-02-07 03:53:09 -0800

    k

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Function to create an object"""
+
+
+def load_from_json_file(filename):
+    """Function to return
+
+    Args:
+        filename: file name.
+    """
+    with open(filename, 'r') as f:
+       obj = f.read()
+       return json.loads(obj)
