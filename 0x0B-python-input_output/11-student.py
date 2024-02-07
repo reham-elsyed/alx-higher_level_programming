9d8ed2187b69bf49dc1a6bd006317dc804b9de4e
reham-elsyed <rehamshepl666@gmail.com>
2024-02-07 15:08:18 -0800

    k

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define class"""
+
+
+class Student:
+    """Class define student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            attrs = vars(self).keys()
+        else:                                                                                   attrs = [attr for attr in attrs if hasattr(self, attr)]                             return {attr: getattr(self, attr) for attr in attrs}
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
