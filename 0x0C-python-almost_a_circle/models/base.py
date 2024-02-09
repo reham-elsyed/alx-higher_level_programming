63668e7d5afabc3240d0dd44c4b2279c2bb72ac0
reham-elsyed <rehamshipl666@gmail.com>
2024-02-09 10:49:09 -0800

    j

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define class"""
+
+
+class Base:
+    """Class base"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
