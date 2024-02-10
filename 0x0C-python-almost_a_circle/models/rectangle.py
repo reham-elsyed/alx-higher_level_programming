aeb027157bb49c9704ed1e1a8f1dfd8e218a101b
reham-elsyed <rehamshepl666@gmail.com>
2024-02-10 11:38:58 -0800

    xy

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,5 +75,7 @@ class Rectangle(Base):
         return self.__width * self.__height
 
     def display(self):
+        for i in range(self.__y):
+            print()
         for i in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
