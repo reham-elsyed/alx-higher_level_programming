d77388a9924af4747fc28cc21d69ab84390ecc92
reham-elsyed <rehamshepl666@gmail.com>
2024-02-11 04:09:22 -0800

    update args

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,6 +16,7 @@ class Rectangle(Base):
 
     def __str__(self):
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+
     @property
     def width(self):
         """Width getter"""
@@ -82,14 +83,11 @@ class Rectangle(Base):
         for i in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
 
-    def update(self, *args):
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+
+    def update(self, *args, **kwargs):
+        if args:
+            for arg in args:
+                setattr(self, arg[0], arg[1])
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
