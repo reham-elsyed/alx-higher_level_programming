ba7058e49c26e14fdbdfa3c02f994f8e0787b796
reham-elsyed <rehamshepl666@gmail.com>
2024-02-13 00:27:04 -0800

    p

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,15 +89,15 @@ class Rectangle(Base):
         """Update arg to attr"""
         if args:
             if args >= 1:
-                setattr(self, "width", args[0])
+                setattr(self, "id", args[0])
             elif args >= 2:
-                setattr(self, "height", args[1])
+                setattr(self, "width", args[1])
             elif args >= 3:
-                setattr(self, "x", args[2])
+                setattr(self, "height", args[2])
             elif args >= 4:
-                setattr(self, "y", args[3])
+                setattr(self, "x", args[3])
             elif args >= 5:
-                setattr(self, "id", args[4])
+                setattr(self, "y", args[4])
         elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
