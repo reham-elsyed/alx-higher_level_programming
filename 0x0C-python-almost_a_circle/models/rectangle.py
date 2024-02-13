43716b5fce7e9aec4658e8f4eb523fb557a88462
reham-elsyed <rehamshepl666@gmail.com>
2024-02-13 01:53:06 -0800

    p

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -108,4 +108,4 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """Return dictionary"""
-    return {"id": self.id, "width": self.width, "height": self.height, "x": self.x, "y": self.y}
+        return {"id": self.id, "width": self.width, "height": self.height, "x": self.x, "y": self.y}
