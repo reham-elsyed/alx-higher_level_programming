8bddc568527a4e403fc5a7de0b974b3c8949affc
reham-elsyed <rehamshepl666@gmail.com>
2024-02-13 01:52:07 -0800

    p

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -105,3 +105,7 @@ class Rectangle(Base):
         elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Return dictionary"""
+    return {"id": self.id, "width": self.width, "height": self.height, "x": self.x, "y": self.y}
