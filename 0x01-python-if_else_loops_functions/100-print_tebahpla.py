910557110d3e74777a9e2604ac17752ba6520dfd
reham-elsyed <rehamshepl666@gmail.com>
2024-02-20 22:25:27 -0800

    alpha

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
-for i in range(ord('z'), ord('a')-1, -1):
-    if i % 2 != 0:
-        print(chr(i).upper(), end='')
-    else:
-        print(chr(i), end='')
+for i in range(ord('z'), ord('a') - 1, -1):
+    print(chr(i).upper() if i % 2 != 0 else chr(i), end='')
