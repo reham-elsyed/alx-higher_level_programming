ea1788a10c969405051e637423c428c25356ecd6
reham-elsyed <rehamshepl666@gmail.com>
2024-02-20 22:36:03 -0800

    alpha

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(ord('z'), ord('a') - 1, -1):
-    print(chr(i).upper() if i % 2 != 0 else chr(i), end='')
+    print("{}".format(chr(i).upper() if i % 2 != 0 else chr(i)), end='')
