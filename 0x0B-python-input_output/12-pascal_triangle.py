e5d85af1db1f2a62e39f0882617648bf1b7b8869
reham-elsyed <rehamshepl666@gmail.com>
2024-03-02 03:00:24 -0800

    o

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""Methob for implementation of pascal triangle"""
+
+
 def pascal_triangle(n):
-    """Method to print pascal triangle"""
-    mylist =[]
+    """Method to print pascal triangle
+
+    Args:
+        n: input number of lines
+
+    Return: list of lists
+    """
+
+    mylist = []
     if numrows <= 0:
         print(mylist)
     else:
@@ -11,7 +21,9 @@ def pascal_triangle(n):
                 if col == 0 or col == row:
                     new_row.append(1)
                 else:
-                    if row - 1 >= 0 and col - 1 >= 0 and row - 1 < len(mylist) and col < len(mylist[row - 1]):
+                    if row - 1 >= 0 and col - 1 >= 0\
+                     and row - 1 < len(mylist)\
+                     and col < len(mylist[row - 1]):
                         val = mylist[row - 1][col - 1] + mylist[row - 1][col]
                         new_row.append(val)
             mylist.append(new_row)
