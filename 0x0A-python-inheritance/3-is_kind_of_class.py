37a40c06066b6b1abbcca2456221648ca3d52167
reham-elsyed <rehamshepl666@gmail.com>
2024-03-02 23:03:46 -0800

    kind of object

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -6,6 +6,7 @@ or if the object is an instance of a class that inherited from
 
 
 def is_kind_of_class(obj, a_class):
+    """Check if element is instance"""
     if isinstance(obj, a_class):
         return True
     else:
