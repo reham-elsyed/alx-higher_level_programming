6d196029b0f65932d4452b4fc9207c938465962f
reham-elsyed <rehamshepl666@gmail.com>
2024-03-02 22:54:16 -0800

    kind of object

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Method to check  the object is an instance of,
+or if the object is an instance of a class that inherited from
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
