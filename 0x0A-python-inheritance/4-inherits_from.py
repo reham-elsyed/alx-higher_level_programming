7c32b4c2caee9fc7e2fe1f85635b5b7bc235135c
reham-elsyed <rehamshepl666@gmail.com>
2024-03-02 23:26:55 -0800

    kind of object

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,7 +6,7 @@ Method to checlinstance of a class that inherited (directly or indirectly)
 
 def inherits_from(obj, a_class):
     """Method to check inheritance"""
-    if isinstance(a_class, obj):
+    if isinstance(obj, a_class):
         return True
     else:
         return False
