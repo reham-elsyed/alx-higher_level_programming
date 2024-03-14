62fb27c64af6d05da5864bfd14159d4f1a589dc0
reham-elsyed <rehamshepl666@gmail.com>
2024-03-13 18:20:17 -0700

    create table isnot null

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,3 @@
--- list all the cities of California from the hbtn_0d_usa
-SELECT cities.name
-FROM cities
-WHERE cities.state_id = (SELECT id FROM states WHERE name = 'California')
+-- list all the citieis of California from the hbtn_0d_usa.
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY cities.id ASC;
