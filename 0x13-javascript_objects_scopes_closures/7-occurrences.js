36927bb0d799740bd780780c045a81184691b2ce
reham-elsyed <rehamshepl666@gmail.com>
2024-04-10 13:49:08 -0700

    l

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-exports.nbOccurences = function (list, searchElement){
-	let inst = 0;
-	let element = 0;
-	while (element < list.length) {
-		if (list[element] === searchElement) {
-			inst++;
-		}
-		element++;
-	}
-	return inst;
-}
+exports.nbOccurences = function (list, searchElement) {
+  let inst = 0;
+  let element = 0;
+  while (element < list.length) {
+    if (list[element] === searchElement) {
+      inst++;
+    }
+    element++;
+  }
+  return inst;
+};
