581186a307f6d9b61194e1aec3fa40d8f6b3b58e
reham-elsyed <rehamshepl666@gmail.com>
2024-04-13 09:41:05 -0700

    filter db

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,26 +5,22 @@ import sys
 
 
 if __name__ == "__main__":
-    '''db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursos()
-    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%'
-                ORDER BY states.id""")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()'''
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
     db = None
     try:
-        db = MySQLdb.connect(host="localhost", user=username,
-                         passwd=password, db=database, port=3306)
+        db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=database,
+            port=3306
+            )
         cur = db.cursor()
-        query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id;"
+        query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' "
+        "ORDER BY states.id;"
         cur.execute(query)
         rows = cur.fetchall()
         for row in rows:
