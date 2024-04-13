16203059516d8b3b8d841189c8933ddaf001a330
reham-elsyed <rehamshepl666@gmail.com>
2024-04-12 19:55:20 -0700

    o

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,28 +1,37 @@
 #!/usr/bin/python3
-"""Module that list all states with n letter"""
-import sys
+'''filter states'''
 import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
-    # Get MYSQL credentials frkm command line arguments
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
-    c = db.cursor()
-
-    # Execute the SQL query to retreive all states by id
-    c.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY 'N%' "
-        "ORDER BY states.id"
-    )
-    rows = c.fetchall()
+    '''db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursos()
+    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%'
+                ORDER BY states.id""")
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-    c.close()
-    db.close()
+    cur.close()
+    db.close()'''
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    db = None
+    try:
+        db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=database, port=3306)
+        cur = db.cursor()
+        query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id;"
+        cur.execute(query)
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
+    finally:
+        if db:
+            db.close()
