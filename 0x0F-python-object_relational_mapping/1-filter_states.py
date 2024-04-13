b04857c9cc5cd12594d5219a22eafad7bf5fe6ce
reham-elsyed <rehamshepl666@gmail.com>
2024-04-12 18:25:48 -0700

    k

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,13 +2,19 @@
 """Module that list all states with n letter"""
 import sys
 import MySQLdb
+import mysql.connector
+
 
 if __name__ == "__main__":
     #Get MYSQL credentials frkm command line arguments
     # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     c = db.cursor()
 
     # Execute the SQL query to retreive all states by id
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+    c.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id""")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
