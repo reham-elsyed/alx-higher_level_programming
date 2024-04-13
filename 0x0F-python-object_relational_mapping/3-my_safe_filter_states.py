2f85a9496c55db4264f0f2b17439f60b0cd9e8d3
reham-elsyed <rehamshepl666@gmail.com>
2024-04-13 11:22:56 -0700

    2filter

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,7 +5,13 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+        )
     c = db.cursor()
     match = sys.argv[4]
     c.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
