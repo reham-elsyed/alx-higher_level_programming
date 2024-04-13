36032c4469836c7c47db41b47a5daf667b2ee29c
reham-elsyed <rehamshepl666@gmail.com>
2024-04-12 19:29:21 -0700

    l

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Method takes in arguments and displays 
-all values in the states"""
+""" Method takes in arguments"""
 import sys
 import MySQLdb
+
 
 if __name__ = "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
