450ea60483413086a5424b45b0fbb959391a3f55
reham-elsyed <rehamshepl666@gmail.com>
2024-04-25 22:23:15 -0700

    o

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  takes in a URL, sends a POST request to the passed
-curl -s "$1" -X POST -d "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD"
+curl -s "$1" -X POST -d "email=test@gmail.com.com&subject=I%20will%20always%20be%20here%20for%20PLD"
