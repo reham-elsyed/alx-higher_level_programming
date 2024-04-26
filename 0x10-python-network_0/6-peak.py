d2244b8f4452e87a6f5a8280107a2a9ac5413086
reham-elsyed <rehamshepl666@gmail.com>
2024-04-25 23:45:36 -0700

    o

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,8 +10,8 @@ def find_peak(list_of_integers):
         peak = 0
         for i, num in enumerate(list_of_integers):
             peak = num
-            if i < len(list_of_integers) - 1 and
-            num > list_of_integers[i + 1] and
+            if i < len(list_of_integers) - 1 and \
+            num > list_of_integers[i + 1] and \
             num > list_of_integers[i - 1]:
                 return num
         return peak
