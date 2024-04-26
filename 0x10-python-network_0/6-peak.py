550ebca999e523eeb82771e263c1e68783546486
reham-elsyed <rehamshepl666@gmail.com>
2024-04-25 23:24:59 -0700

    o

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Method to find peak in an unsorted list"""
+
+
+def find_peak(list_of_integers):
+    if len(list_of_integers) == 0:
+        return None
+    else:
+        peak = 0
+        for i, num in enumerate(list_of_integers):
+            peak  = num
+            if i < len(list_of_integers) - 1 and num > list_of_integers[i + 1] and num > list_of_integers[i - 1]:
+                return num
+        return peak
