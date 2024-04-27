69633ede95282c62381609e3ddc968329196a90b
reham-elsyed <rehamshepl666@gmail.com>
2024-04-27 08:57:29 -0700

    l

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" script that takes in a URL, sends a request to the URL and displays"""
+
+
+if __name__ == "__main__":
+    from urllib import request, error
+    import sys
+
+    url = request.Request(sys.argv[1])
+
+    try:
+        with request.urlopen(url) as res:
+        print(res.read().decoded('utf-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
