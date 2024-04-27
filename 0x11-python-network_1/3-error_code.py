e014de6e822a9a375b5cefce6c8070b715259bbd
reham-elsyed <rehamshepl666@gmail.com>
2024-04-27 09:24:01 -0700

    l

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script that takes in a URL, sends a request to the URL and displays"""
+"""Script that takes in a URL, sends a request to the URL and displays"""
 
 
 if __name__ == "__main__":
