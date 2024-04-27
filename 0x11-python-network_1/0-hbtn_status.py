cf50f15fa0a94d8eb95e88fae3e38352c19d9a0b
reham-elsyed <rehamshepl666@gmail.com>
2024-04-27 05:06:25 -0700

    fitsh data

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """ Method to fetch website"""
-import urlib.request
 
 
 if __name__ == '__main__':
-    import urlib.request
-    with urlib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    import urllib.request
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".fortmat(html))
+        print("\t- content: {}".format(html))
         print("\t- utf8 content: {}".format(html.decode('utf-8')))
