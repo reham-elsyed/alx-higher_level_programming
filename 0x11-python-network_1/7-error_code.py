c7a48466b011cea238b620606ef3cd1364b06d2b
reham-elsyed <rehamshepl666@gmail.com>
2024-04-28 12:26:53 -0700

    get statue

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     r = requests.get(url)
-        if r.status_code >= 400:
-            print("Error code: {}".format(r.status_code))
-        else:
-            print(r.text)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
